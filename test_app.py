0d332df24c78a662385a30a3cdcf46b1fcfeaafa
paultriton <paulorikiratriton@gmail.com>
2018-05-31 13:57:49 +0300

    [finishes #157962116] modify a request

--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
-"""test_get.py"""
+
 """tests_app.py"""
 import unittest
 import json
@@ -7,28 +7,28 @@ from app import APP
 class FlaskTestCase(unittest.TestCase):
     """Test for app.py"""
 
-    def test_get_one(self):
-        """test get one"""
+    def test_get_request(self):
+        """test get two request"""
 
         test_client = APP.test_client(self)
 
         response = test_client.put(
-            '/api/v1/users/requests/1',
+            '/api/v1/users/requests/93',
             data=json.dumps({"request":"helo"}), content_type="application/json"
         )
 
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Request modified succesfully", response.data)
+        self.assertIn(b"modified request succesfully", response.data)
 
-    
-    def test_failed_get_one(self):
-        """test not found request to modify"""
+
+    def test_failed_get_request(self):
+        """test not found to modify"""
 
         test_client = APP.test_client(self)
 
         response = test_client.put(
-            '/api/v1/users/requests/23',
-            data=json.dumps({"request":"helo"}), content_type="application/json"
+            '/api/v1/users/requests/93',
+            data=json.dumps({"request":"hello"}), content_type="application/json"
         )
 
-        self.assertIn(b"Request not found!", response.data)
+        self.assertIn(b"no request found", response.data)
