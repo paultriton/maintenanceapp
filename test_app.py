d36910a95c5cbc7d57a538d72d81b7ce3c90a00a
paultriton <paulorikiratriton@gmail.com>
2018-05-31 13:05:52 +0300

    [finishes #157961978] get one request for logged in user

--- test_app.py
+++ test_app.py
@@ -4,8 +4,14 @@ from app import APP
 
 class AppTests(unittest.TestCase):
     """Class for my tests"""
-    def test_get_all(self):
-        """Test get method"""
-        tester = APP.test_client(self)
-        response = tester.get('/api/v1/users/requests')
+    def test_get_one(self):
+        """Test get one method"""
+        test_client = APP.test_client(self)
+        response = test_client.get('/api/v1/users/requests/2')
         self.assertEqual(response.status_code, 200)
+
+    def test_get_one_not_found(self):
+        """Test get one method"""
+        test_client = APP.test_client(self)
+        response = test_client.get('/api/v1/users/requests/23')
+        self.assertIn(b"no request found", response.data)
